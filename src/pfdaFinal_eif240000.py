58c0a3f056bf08812b70f7a821bcab2dbc692599
eif240000 <eif240000@utdallas.edu>
2026-04-23 17:48:19 -0500

    "create_nodes" has integrated the previous "create_ai_stand" function. It creates an "aiStandardSurface", "place2dTexture", "file" and connects them properly.

--- src/pfdaFinal_eif240000.py
+++ src/pfdaFinal_eif240000.py
@@ -1,12 +1,12 @@
 import maya.cmds as cmds
 import os
 
-def textures_path():
-    path = cmds.fileDialog2(dialogStyle=2, caption="Select texture directory", fileMode=3)
-    if not path:
-       cmds.warning("Operation cancelled.")
+#def textures_path():
+    #baseColor = ["basecolor", "albedo", "diffuse", "color"]
+    #path = cmds.fileDialog2(dialogStyle=2, caption="Select texture directory", fileMode=3)
+    #if not path:
+       #cmds.warning("Operation cancelled")
 
-    
 def dialog_box():
     shader_name = cmds.promptDialog(
 		title='Specify Shader Name',
@@ -19,21 +19,26 @@ def dialog_box():
         user_input = cmds.promptDialog(query=True, text=True)
         return user_input
     else:
-        cmds.warning("Operation cancelled.")
-        
- 
-def create_ai_stand(name):
-    create_shader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
-    return create_shader
+        cmds.warning("Operation cancelled")
+
+def create_nodes(name):
+    shader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
+    place_2d = cmds.shadingNode('place2dTexture', asUtility=True)
+    diffuse_tex = cmds.shadingNode('file', asTexture=True, name="diffuse_TEX")
+    
+    cmds.connectAttr(place_2d + ".outUV", diffuse_tex + ".uvCoord")
+    cmds.connectAttr(place_2d + ".outUvFilterSize", diffuse_tex + ".uvFilterSize")
+    cmds.connectAttr(diffuse_tex + ".outColor", shader + ".baseColor")
+    return create_nodes
 
 def main():
-   user_path = textures_path()
-   if not user_path:
-       return None
+   #user_path = textures_path()
+   #if not user_path:
+       #return None
    custom_name = dialog_box()
    if not custom_name:
         return None
-   create_shader = create_ai_stand(custom_name)
+   create_shader = create_nodes(custom_name)
 
     
 if __name__ == "__main__":
