3cc1ec4dbbf4cd610ba499801d37be28d08c3034
eif240000 <eif240000@utdallas.edu>
2026-04-23 16:47:25 -0500

    Updated naming convention.

--- src/pfdaFinal_eif240000.py
+++ src/pfdaFinal_eif240000.py
@@ -1,39 +1,39 @@
 import maya.cmds as cmds
 import os
 
-def texturesPath():
+def textures_path():
     path = cmds.fileDialog2(dialogStyle=2, caption="Select texture directory", fileMode=3)
     if not path:
-        cmds.warning("Operation cancelled.")
+       cmds.warning("Operation cancelled.")
 
-
-def dialogBox():
-    shaderName = cmds.promptDialog(
+    
+def dialog_box():
+    shader_name = cmds.promptDialog(
 		title='Specify Shader Name',
 		button=['OK', 'Cancel'],
 		cancelButton='Cancel',
 		message='Enter Shader Name:'
     )
 
-    if shaderName == 'OK':
-        userInput = cmds.promptDialog(query=True, text=True)
-        return userInput
+    if shader_name == 'OK':
+        user_input = cmds.promptDialog(query=True, text=True)
+        return user_input
     else:
         cmds.warning("Operation cancelled.")
         
  
-def createAiStand(name):
-    createShader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
-    return createShader
+def create_ai_stand(name):
+    create_shader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
+    return create_shader
 
 def main():
-    userPath = texturesPath()
-    if not userPath:
+   user_path = textures_path()
+   if not user_path:
+       return None
+   custom_name = dialog_box()
+   if not custom_name:
         return None
-    customName = dialogBox()
-    if not customName:
-        return None
-    createShader = createAiStand(customName)
+   create_shader = create_ai_stand(custom_name)
 
     
 if __name__ == "__main__":
