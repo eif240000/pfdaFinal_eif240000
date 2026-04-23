8ed3f873b6d456a0d93110625ed41eaea1f5f751
eif240000 <eif240000@utdallas.edu>
2026-04-23 15:04:50 -0500

    "def texturesPath" prompts user to specify a directory for textures and cancels with a warning message.

--- src/pfdaFinal_eif240000.py
+++ src/pfdaFinal_eif240000.py
@@ -1,6 +1,12 @@
 import maya.cmds as cmds
 import os
 
+def texturesPath():
+    path = cmds.fileDialog2(dialogStyle=2, caption="Select texture directory", fileMode=3)
+    if not path:
+        cmds.warning("Operation cancelled.")
+
+
 def dialogBox():
     shaderName = cmds.promptDialog(
 		title='Specify Shader Name',
@@ -14,12 +20,16 @@ def dialogBox():
         return userInput
     else:
         cmds.warning("Operation cancelled.")
+        
  
 def createAiStand(name):
     createShader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
     return createShader
 
 def main():
+    userPath = texturesPath()
+    if not userPath:
+        return None
     customName = dialogBox()
     if not customName:
         return None
