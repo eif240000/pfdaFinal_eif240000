e71f6fd91929be7ce4ed34a34d90a31a53d0d8bd
eif240000 <eif240000@utdallas.edu>
2026-05-08 17:35:09 -0500

    Fully implemented the "textures_path" function as "find_diffuse."
    Instead of implementing  "path finding" into the "find_diffuse" function, it's placed into the "user_path" function, which prompts a user to input the path to the folder with textures.
    
    "create_nodes" correctly returns "shader."
    
    Changed some names of functions and other items to reflect what they do more accurately.

--- src/pfdaFinal_eif240000.py
+++ src/pfdaFinal_eif240000.py
@@ -1,12 +1,21 @@
 import maya.cmds as cmds
 import os
 
-#def textures_path():
-    #baseColor = ["basecolor", "albedo", "diffuse", "color"]
-    #path = cmds.fileDialog2(dialogStyle=2, caption="Select texture directory", fileMode=3)
-    #if not path:
-       #cmds.warning("Operation cancelled")
-
+def user_path():
+    path = cmds.fileDialog2(dialogStyle=2, caption="Select texture directory", fileMode=3)
+    if not path:
+       cmds.warning("Operation cancelled")
+    return path[0]
+       
+def find_diffuse(path):
+    diffuse_names = ["basecolor", "albedo", "diffuse", "color"]
+    
+    for file in os.listdir(path):
+        lower = file.lower()
+        for name in diffuse_names:
+            if name in lower:
+                return os.path.join(path, file)
+    
 def dialog_box():
     shader_name = cmds.promptDialog(
 		title='Specify Shader Name',
@@ -21,24 +30,27 @@ def dialog_box():
     else:
         cmds.warning("Operation cancelled")
 
-def create_nodes(name):
+def create_nodes(name, diffuse_path):
     shader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
     place_2d = cmds.shadingNode('place2dTexture', asUtility=True)
     diffuse_tex = cmds.shadingNode('file', asTexture=True, name="diffuse_TEX")
     
+    cmds.setAttr(diffuse_tex + ".fileTextureName", diffuse_path, type="string")
+    
     cmds.connectAttr(place_2d + ".outUV", diffuse_tex + ".uvCoord")
     cmds.connectAttr(place_2d + ".outUvFilterSize", diffuse_tex + ".uvFilterSize")
     cmds.connectAttr(diffuse_tex + ".outColor", shader + ".baseColor")
-    return create_nodes
+    return shader
 
 def main():
-   #user_path = textures_path()
-   #if not user_path:
-       #return None
    custom_name = dialog_box()
    if not custom_name:
-        return None
-   create_shader = create_nodes(custom_name)
+       return None
+   custom_path = user_path()
+   if not custom_path:
+       return None
+   user_diffuse = find_diffuse(custom_path)
+   create_nodes(custom_name, user_diffuse)
 
     
 if __name__ == "__main__":
