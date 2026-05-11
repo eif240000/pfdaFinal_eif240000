2b9e80b3c290f482e155a138b188cbbb9cadc7f3
eif240000 <eif240000@utdallas.edu>
2026-05-11 15:06:14 -0500

    Code now correctly finds and attaches textures to their respective file node.

--- src/pfdaFinal_eif240000.py
+++ src/pfdaFinal_eif240000.py
@@ -7,16 +7,33 @@ def user_path():
        cmds.warning("Operation cancelled")
     return path[0]
        
-def find_diffuse(path):
-    diffuse_names = ["basecolor", "albedo", "diffuse", "color"]
+def find_textures(path, keywords):
+    if not path:
+        return None
     
     for file in os.listdir(path):
         lower = file.lower()
-        
-        for name in diffuse_names:
+        for name in keywords:
             if name in lower:
                 return os.path.join(path, file)
+    return None
+                
+def find_diffuse(path):
+    diffuse_names = ["basecolor", "albedo", "diffuse", "color"]
+    return find_textures(path, diffuse_names)
     
+def find_roughness(path):
+    roughness_names = ["roughness", "rough", "gloss", "spec", "specular"]
+    return find_textures(path, roughness_names)
+    
+def find_normal(path):
+    normal_names = ["normal", "norm", "nrm"]
+    return find_textures(path, normal_names)
+    
+def find_metalness(path):
+    metalness_names = ["metalness","metal", "metallic"]
+    return find_textures(path, metalness_names)
+
 def dialog_box():
     shader_name = cmds.promptDialog(
 		title='Specify Shader Name',
@@ -31,7 +48,7 @@ def dialog_box():
     else:
         cmds.warning("Operation cancelled")
 
-def create_nodes(name, diffuse_path):
+def create_nodes(name, diffuse_path, roughness_path, metalness_path, normal_path):
     shader = cmds.shadingNode('aiStandardSurface', asShader=True, name=name)
     place_2d = cmds.shadingNode('place2dTexture', asUtility=True)
     normal_map = cmds.shadingNode('aiNormalMap', asUtility=True)
@@ -40,7 +57,11 @@ def create_nodes(name, diffuse_path):
     metal_tex = cmds.shadingNode('file', asTexture=True, name="metal_TEX")
     normal_tex = cmds.shadingNode('file', asTexture=True, name="normal_TEX")
     
+    
     cmds.setAttr(diffuse_tex + ".fileTextureName", diffuse_path, type="string")
+    cmds.setAttr(normal_tex + ".fileTextureName", normal_path, type="string")
+    cmds.setAttr(roughness_tex + ".fileTextureName", roughness_path, type="string")
+    cmds.setAttr(metal_tex + ".fileTextureName", metalness_path, type="string")
     
     cmds.connectAttr(place_2d + ".outUV", diffuse_tex + ".uvCoord")
     cmds.connectAttr(place_2d + ".outUvFilterSize", diffuse_tex + ".uvFilterSize")
@@ -63,17 +84,19 @@ def create_nodes(name, diffuse_path):
     return shader
 
 def main():
-   custom_name = dialog_box()
+   custom_name = dialog_box() 
    if not custom_name:
        return None
+       
    custom_path = user_path()
    if not custom_path:
        return None
-   user_diffuse = find_diffuse(custom_path)
-   if not user_diffuse:
-       cmds.warning("No diffuse texture found")
-       return None
-   create_nodes(custom_name, user_diffuse)
+       
+   diffuse_path = find_diffuse(custom_path)
+   roughness_path = find_roughness(custom_path)
+   metalness_path = find_metalness(custom_path)
+   normal_path = find_normal(custom_path)     
+   create_nodes(custom_name, diffuse_path, roughness_path, metalness_path, normal_path)
 
     
 if __name__ == "__main__":
